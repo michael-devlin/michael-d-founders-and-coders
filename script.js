e00cf3becddfbcc3ae0c4e5681f0b9596d2e413d
mikailbadoula <michaeldevfay@mgmail.com>
2020-12-04 11:33:33 +0000

    Add more quotes to Quote Generator function

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function newQuote() {
 The truth is far more frightening - Nobody is in control.
 
 The world is rudderless.”',
-― Alfred Huang: '“When things reach the extreme, they alternate to the opposite.”',
+― Ursula Le Guin: '“It is good to have an end to journey toward; but it is the journey that matters, in the end.”',
 - Ayishat Akanbi: '"It’s naive to think that you wouldn’t have taken part in historical atrocities once considered normal if you embrace all the trendy ideas of today."',
 ― Martin Luther King Jr. '“I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their character.”',
 ― Sam Harris: '“Consider it: every person you have ever met, every person will suffer the loss of his friends and family. All are going to lose everything they love in this world. Why would one want to be anything but kind to them in the meantime?”',
@@ -24,10 +24,25 @@ The world is rudderless.”',
 ― Lao Tzu: '“When I let go of what I am, I become what I might be.”',
 ― Buddha: '“You only lose what you cling to.”',
 ― Thich Nhat Hanh: '“To be beautiful means to be yourself. You don’t need to be accepted by others. You need to accept yourself.”',
-― Dalai Lama: '"I’m Tibetan, I’m Buddhist and I’m the Dalai Lama, but if I emphasize these differences it sets me apart and raises barriers with other people. What we need to do is to pay more attention to the ways in which we are the same as other people.""',
+― Dalai Lama: '"I’m Tibetan, I’m Buddhist and I’m the Dalai Lama, but if I emphasize these differences it sets me apart and raises barriers with other people. What we need to do is to pay more attention to the ways in which we are the same as other people."',
 ― Ayishat Akanbi: '"It’s not empathy if you can <i>only</i> direct it to those you deem unfortunate. It’s just moralistic pity which often stems from an inferiority complex, guilt, and the desperation to be perceived as ‘good’.'",
 ― Maya Angelou: '“Hate, it has caused a lot of problems in the world, but has not solved one yet.”',
 ― Deeyah Khan: '"Self-expression should not be a challenge that demands extraordinary talent but should be a right accessible to all."',
-― Peter Singer: '“We are, quite literally, gambling with the future of our planet - for the sake of hamburgers”'
+― Peter Singer: '“We are, quite literally, gambling with the future of our planet - for the sake of hamburgers”',
+― Elif Shafak: '“Do not go with the flow. Be the flow.”',
+― Hermann Hesse: '“Whoever wants music instead of noise, joy instead of pleasure, soul instead of gold, creative work instead of business, passion instead of foolery, finds no home in this trivial world of ours.”',
+― Ramana Maharshi: "“Wanting to reform the world without discovering one's true self is like trying to cover the world with leather to avoid the pain of walking on stones and thorns. It is much simpler to wear shoes.”",
+― Nisargadatta Maharaj: '“You will receive everything you need when you stop asking for what you do not need”',
+― Bhagavad Gita II, 47: '“Be intent on action, not on the fruits of action, avoid attraction to the fruits and attachment to inaction!"'
   }
+
+  // get all keys (authors)
+  var keys = Object.keys(quotes);
+  // get a random key (author) and store it in author
+  var author = keys[Math.floor(Math.random() * keyslength)];
+  // get the value (quote) that belongs to that key
+  var quote = quotes[author-name]
+
+  document.getElementById("quote").innerHTML = quote;
+  document.getElementById("author-name").innerHTML = author-name;
 }
